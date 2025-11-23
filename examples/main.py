193b3366388759743afccca8cea0c4a113b033a1
Mapi <matvey2000000009@gmail.com>
2025-11-23 02:36:46 +0100

    issues fixes

--- examples/main.py
+++ examples/main.py
@@ -1,5 +1,5 @@
 from dapo import DataKit
-import tests.test_data as td
+import example_data as td
 
 datasets_src = "src/datasets"
 
@@ -8,8 +8,6 @@ dk_1 = DataKit.from_toon(f"{datasets_src}/jobs.toon")
 # dk_1.add_column("latitude", [10, 5, 2, 46, 7, 8, 3, 6, 10, 10])
 # dk_1.add_column("test", [10, 5, 2, 46, 7, 8, 3, 6, 10, 10])
 
-print(dk_1.sort(["Country"]))
-print(dk_1.head().get_column("Country"))
-print(dk_1.tail().get_column("Country"))
+dk_1 = dk_1.select(["Country", "Job Title"]).group_by("Country", {"Country": "count"}).sort("Country").filter(lambda x: x["Country"] < "G" and x["count_Country"] > 6)
 
 dk_1.to_csv(f"{datasets_src}/test.csv")
